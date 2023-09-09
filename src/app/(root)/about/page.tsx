a109a3540690a10858ad3c5b783801d31c1c5acd
Ezequiel Parziale <eparziale@gmail.com>
2023-09-08 23:06:02 -0300

    :rocket: update: add about page

---
+++ src/app/(root)/about/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/container"
+
+export default function About() {
+  return (
+    <>
+      <Container>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          About
+        </h1>
+      </Container>
+    </>
+  )
+}
