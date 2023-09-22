b2cef94490586577517ad050c7722f2b96d1686c
Ezequiel Parziale <eparziale@gmail.com>
2023-09-22 19:19:26 -0300

    :rocket: update: add profile and settings pages

---
+++ src/app/(root)/settings/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/container"
+
+export default async function Settings() {
+  return (
+    <>
+      <Container>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Settings
+        </h1>
+      </Container>
+    </>
+  )
+}
