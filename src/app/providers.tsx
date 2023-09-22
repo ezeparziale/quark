9b6d8826e8b1aa89e1d133828f0697b94985748f
Ezequiel Parziale <eparziale@gmail.com>
2023-09-22 19:14:30 -0300

    :rocket: update: add session provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from "next-themes"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-      </ThemeProvider>
+      <SessionProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </SessionProvider>
     </>
   )
 }
