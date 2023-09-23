c9796a8a35c37770c8694b4d18c9a5ce668cf890
Ezequiel Parziale <eparziale@gmail.com>
2023-09-23 16:35:11 -0300

    :lipstick: update: improve navbar
    - height
    - transparent

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,9 +3,9 @@ import MobileNav from "./mobileNav"
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b dark:border-b-neutral-800">
+    <header className="sticky top-0 z-50 w-full border-b border-muted bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <nav
-        className="container flex h-14 max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8"
+        className="container flex h-16 max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8"
         aria-label="Global"
       >
         <DesktopNav />
