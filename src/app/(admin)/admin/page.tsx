f46afce587fe015407aa30029279185925f6ba0f
Ezequiel Parziale <eparziale@gmail.com>
2023-10-16 20:01:17 -0300

    :rocket: update: add admin page

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/container"
+
+export default async function AdminPage() {
+  return (
+    <>
+      <Container>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Admin
+        </h1>
+      </Container>
+    </>
+  )
+}
