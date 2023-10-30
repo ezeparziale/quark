2dbbde1fd5b0c64cefd0f9b285f9a137fde83ce3
Ezequiel Parziale <eparziale@gmail.com>
2023-10-29 21:19:43 -0300

    :rocket: update: add rbac module

---
+++ src/lib/rbac.tsx
@@ -0,0 +1,35 @@
+import { redirect } from "next/navigation"
+
+import { getCurrentUser } from "@/actions/getCurrentUser"
+import prismadb from "@/utils/prismadb"
+
+async function hasRequiredPermissions(requiredPermissions: string[]): Promise<boolean> {
+  const user = await getCurrentUser()
+  if (user) {
+    const userRoles = await prismadb.userRole.findMany({
+      where: { userId: user.id },
+      include: {
+        role: { include: { permissions: { include: { permission: true } } } },
+      },
+    })
+    return userRoles.some((userRole) => {
+      return requiredPermissions.some((permission) => {
+        return userRole.role.permissions.some(
+          (rolePermission) => rolePermission.permission.name === permission,
+        )
+      })
+    })
+  }
+  return false
+}
+
+export function withRoles(Component: any, requiredPermissions: string[]) {
+  return async function WithRolesWrapper(props: any) {
+    const hasPermission = await hasRequiredPermissions(requiredPermissions)
+    if (hasPermission) {
+      return <Component {...props} />
+    } else {
+      redirect("/auth/error/?error=AccessUnauthorized")
+    }
+  }
+}
