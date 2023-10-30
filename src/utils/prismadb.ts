d17ca744a6e3ec0af208953da1777d1e2fccf736
Ezequiel Parziale <eparziale@gmail.com>
2023-10-30 10:27:43 -0300

    :card_file_box: update: set prisma logs

--- src/utils/prismadb.ts
+++ src/utils/prismadb.ts
@@ -4,7 +4,12 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prismadb = globalThis.prisma || new PrismaClient()
+const prismadb =
+  globalThis.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  })
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
 export default prismadb
