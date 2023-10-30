39f7b8ed54b6e28a2e7742ca7398aaf9ffafa898
Ezequiel Parziale <eparziale@gmail.com>
2023-10-30 11:58:38 -0300

    :zap: improve: check env vars on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import "./src/env.mjs"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,4 +7,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
