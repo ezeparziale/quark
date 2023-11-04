e281181c7d6ecea35af978824938746d55014f0e
Ezequiel Parziale <eparziale@gmail.com>
2023-11-03 22:10:46 -0300

    :rocket: update: add illustrations not found user

--- src/app/(admin)/admin/users/[user_id]/not-found.tsx
+++ src/app/(admin)/admin/users/[user_id]/not-found.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image"
 import Link from "next/link"
+
+import { ArrowLeft } from "lucide-react"
 
 import Container from "@/components/container"
 import { Button } from "@/components/ui/button"
@@ -6,17 +9,31 @@ import { Button } from "@/components/ui/button"
 export default function UserNotFound() {
   return (
     <Container>
-      <div className="flex h-screen flex-col items-center justify-center">
-        <div className="rounded-lg border p-4">
-          <div className="flex flex-col space-y-6">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-50 md:text-2xl">
-              Oops! User not found
-            </h2>
-            <Button asChild variant="default" size="sm" className="font-bold">
-              <Link href="/admin/users">Back to users</Link>
-            </Button>
-          </div>
-        </div>
+      <div className="mt-20 flex flex-col items-center space-y-2">
+        <h1 className="text-4xl">404</h1>
+        <Image
+          alt="missing site"
+          src="/not-found/falling-gray.svg"
+          width={400}
+          height={400}
+          className="dark:hidden"
+        />
+        <Image
+          alt="missing site"
+          src="/not-found/falling-dark.svg"
+          width={400}
+          height={400}
+          className="hidden dark:block"
+        />
+        <h2 className="text-2xl font-bold tracking-tight md:text-3xl">
+          Oops! User not found
+        </h2>
+        <Button asChild variant="default" size="sm">
+          <Link href="/admin/users">
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            Back to users
+          </Link>
+        </Button>
       </div>
     </Container>
   )
