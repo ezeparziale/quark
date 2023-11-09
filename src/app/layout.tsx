2bc3358239b5f70f098098f01dca0b205f2f5c41
Ezequiel Parziale <eparziale@gmail.com>
2023-11-09 20:59:41 -0300

    :lipstick: update: use GeistSans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import { GeistSans } from "geist/font/sans"
 
 import { Toaster } from "@/components/ui/toaster"
 
 import "@/styles/globals.css"
 
 import Providers from "./providers"
-
-const font = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Quark",
@@ -19,7 +17,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background antialiased", font.className)}>
+      <body
+        className={cn("min-h-screen bg-background antialiased", GeistSans.className)}
+      >
         <div className="relative flex min-h-screen flex-col">
           <Providers>
             <div className="flex-1">{children}</div>
