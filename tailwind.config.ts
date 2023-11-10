829f2623a2b28e8c8783e3f8c0a6ce17f451944a
Ezequiel Parziale <eparziale@gmail.com>
2023-11-09 21:17:32 -0300

    :rocket: update: change format export

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss"
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -75,5 +74,3 @@ const config: Config = {
   },
   plugins: [require("tailwindcss-animate")],
 }
-
-export default config
