6d3339779958944d6fd28686f5d070f2559c2643
Ezequiel Parziale <eparziale@gmail.com>
2023-11-12 20:45:56 -0300

    :rocket: update: add edit user action

---
+++ src/actions/users/edit-user.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import { DataResult } from "@/types/types"
+import prismadb from "@/utils/prismadb"
+
+interface IUser {
+  id: string
+  username: string
+  email: string
+  active: boolean
+  confirmedEmail: boolean
+}
+
+export async function editUser({
+  id,
+  username,
+  email,
+  active,
+  confirmedEmail,
+}: IUser): Promise<DataResult<IUser>> {
+  try {
+    const errors: { email: string[]; username: string[] } = {
+      email: [],
+      username: [],
+    }
+    const emailAlreadyExists = await prismadb.user.findUnique({
+      where: { email, NOT: { id } },
+    })
+    if (emailAlreadyExists) {
+      errors.email.push(`An account with the email ${email} already exists.`)
+    }
+
+    const usenameAlreadyExists = await prismadb.user.findUnique({
+      where: { username, NOT: { id } },
+    })
+    if (usenameAlreadyExists) {
+      errors.username.push("Username already exists.")
+    }
+
+    if (errors.email.length || errors.username.length) {
+      return { success: false, errors }
+    }
+
+    await prismadb.user.update({
+      where: { id },
+      data: { username, email, active, confirmedEmail, hashedPassword: "" },
+    })
+    return { success: true }
+  } catch (error) {
+    return { success: false }
+  }
+}
