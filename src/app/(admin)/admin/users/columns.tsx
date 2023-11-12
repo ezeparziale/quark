18f9920326252c40db190989844a14425b4d2c3a
Ezequiel Parziale <eparziale@gmail.com>
2023-11-12 20:48:13 -0300

    :rocket: update: add new columns

--- src/app/(admin)/admin/users/columns.tsx
+++ src/app/(admin)/admin/users/columns.tsx
@@ -3,6 +3,7 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowDown, ArrowUp, ArrowUpDown } from "lucide-react"
 
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 
 import CellActions from "./_components/cell-actions"
@@ -11,6 +12,8 @@ export interface IUsersColumns {
   id: string
   email: string
   username: string
+  active: boolean
+  confirmedEmail: boolean
 }
 
 export const columns: ColumnDef<IUsersColumns>[] = [
@@ -52,6 +55,60 @@ export const columns: ColumnDef<IUsersColumns>[] = [
           )}
         </Button>
       )
+    },
+  },
+  {
+    accessorKey: "active",
+    id: "active",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Active
+          {column.getIsSorted() === false ? (
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          ) : column.getIsSorted() === "asc" ? (
+            <ArrowUp className="ml-2 h-4 w-4" />
+          ) : (
+            <ArrowDown className="ml-2 h-4 w-4" />
+          )}
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const value = row.original.active
+      const badge = (
+        <Badge variant={value ? "success" : "danger"}>{String(value)}</Badge>
+      )
+      return badge
+    },
+  },
+  {
+    accessorKey: "confirmedEmail",
+    id: "confirmedEmail",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Confirmed Email
+          {column.getIsSorted() === false ? (
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          ) : column.getIsSorted() === "asc" ? (
+            <ArrowUp className="ml-2 h-4 w-4" />
+          ) : (
+            <ArrowDown className="ml-2 h-4 w-4" />
+          )}
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const value = row.original.confirmedEmail
+      const badge = <Badge variant={value ? "info" : "warning"}>{String(value)}</Badge>
+      return badge
     },
   },
   {
