8bf363267c068c0e62c716e528835605636322da
Ezequiel Parziale <eparziale@gmail.com>
2023-11-13 19:35:22 -0300

    :bug: fix: hide header in create role

--- src/app/(admin)/admin/roles/[roleId]/page.tsx
+++ src/app/(admin)/admin/roles/[roleId]/page.tsx
@@ -35,13 +35,19 @@ const RoleAdminPage = async ({ params }: { params: { roleId: string } }) => {
   return (
     <>
       <div className="space-y-6">
-        <div className="flex items-center justify-between">
-          <div className="space-y-0.5">
-            <h3 className="text-lg font-medium">Settings</h3>
-            <p className="text-sm text-muted-foreground">Manage role settings</p>
-          </div>
-        </div>
-        <Separator />
+        {role ? (
+          <>
+            <div className="flex items-center justify-between">
+              <div className="space-y-0.5">
+                <h3 className="text-lg font-medium">Settings</h3>
+                <p className="text-sm text-muted-foreground">Manage role settings</p>
+              </div>
+            </div>
+            <Separator />
+          </>
+        ) : (
+          <></>
+        )}
         <RoleForm role={role} />
       </div>
     </>
