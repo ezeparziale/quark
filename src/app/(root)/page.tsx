b7856f3c4757cac256c690e9a74e00572210299b
Ezequiel Parziale <eparziale@gmail.com>
2023-11-15 20:34:55 -0300

    :rocket: update: add home title

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,32 @@
+import Link from "next/link"
+
+import { ArrowRight } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Home
-      </h1>
+      <section className="w-full py-12 md:py-24 lg:py-32">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col items-center space-y-4 text-center">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">
+              Welcome to{" "}
+              <span className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 bg-clip-text text-transparent">
+                Quark
+              </span>
+            </h1>
+            <p className="max-w-[600px] text-foreground md:text-xl">
+              Your template for Next.js to create tools faster
+            </p>
+            <Button size={"lg"} asChild>
+              <Link href="/auth/login">
+                Get Started <ArrowRight className="ml-3 h-5 w-5" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
