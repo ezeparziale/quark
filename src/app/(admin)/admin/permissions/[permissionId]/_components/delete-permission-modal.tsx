d79da5b47711eff815720b837b1f84fc5ab45e2c
Ezequiel Parziale <eparziale@gmail.com>
2023-12-28 22:16:21 -0300

    :zap: improve: modal for delete permission
    - add confirm with form value

--- src/app/(admin)/admin/permissions/[permissionId]/_components/delete-permission-modal.tsx
+++ src/app/(admin)/admin/permissions/[permissionId]/_components/delete-permission-modal.tsx
@@ -5,8 +5,12 @@ import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 import { deletePermission } from "@/actions/permissions/delete"
+import { zodResolver } from "@hookform/resolvers/zod"
 import type { Permission } from "@prisma/client"
-import { Trash2 } from "lucide-react"
+import { Loader2, Trash2 } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import * as z from "zod"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -18,10 +22,40 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
 
 export default function DeleteUserModal({ permission }: { permission: Permission }) {
   const router = useRouter()
   const [isOpen, setIsOpen] = useState(false)
+
+  const formSchema = z.object({
+    confirmString: z.literal(permission.key, {
+      errorMap: () => ({ message: "Incorrect permission key" }),
+    }),
+  })
+
+  type FormData = z.infer<typeof formSchema>
+
+  const form = useForm<FormData>({
+    defaultValues: { confirmString: "" },
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = async (data: FormData) => {
+    await deletePermission(permission)
+    setIsOpen(false)
+    router.push("/admin/permissions")
+    toast.success("Permission deleted successfully!")
+  }
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -36,33 +70,61 @@ export default function DeleteUserModal({ permission }: { permission: Permission
         <DialogHeader>
           <DialogTitle>Delete permission</DialogTitle>
           <DialogDescription>
-            Are you sure to delete permission:{" "}
-            <span className="font-bold">{permission.name}</span>
+            This action is permanent and could result in users losing access to your
+            application.
           </DialogDescription>
         </DialogHeader>
-        <DialogFooter>
-          <Button
-            variant="secondary"
-            onClick={() => {
-              setIsOpen(false)
-            }}
-            type="button"
-          >
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            variant="destructive"
-            onClick={() => {
-              deletePermission(permission)
-              setIsOpen(false)
-              router.push("/admin/permissions")
-              router.refresh()
-            }}
-          >
-            Delete
-          </Button>
-        </DialogFooter>
+        <Separator />
+        <div className="flex flex-col items-start space-y-4">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex w-full flex-col space-y-4"
+            >
+              <FormField
+                control={form.control}
+                name="confirmString"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormDescription>
+                      Enter the permission key <b>{permission.key}</b> to continue.
+                    </FormDescription>
+                    <FormControl>
+                      <Input
+                        placeholder=""
+                        {...field}
+                        disabled={form.formState.isSubmitting}
+                        autoComplete="no"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <Button
+                  variant="ghost"
+                  onClick={() => {
+                    setIsOpen(false)
+                  }}
+                  type="button"
+                >
+                  Cancel
+                </Button>
+                <Button
+                  variant="destructive"
+                  disabled={form.formState.isSubmitting || !form.formState.isDirty}
+                  type="submit"
+                >
+                  {form.formState.isSubmitting && (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Delete permission
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </div>
       </DialogContent>
     </Dialog>
   )
