b48399f50c48d4c844892b60ed10984c60e929ef
Ezequiel Parziale <eparziale@gmail.com>
2023-12-31 14:55:38 -0300

    :rocket: update: add functions to get data from db

---
+++ src/data/user.ts
@@ -0,0 +1,31 @@
+import prismadb from "@/utils/prismadb"
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await prismadb.user.findUnique({ where: { email } })
+
+    return user
+  } catch {
+    return null
+  }
+}
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await prismadb.user.findUnique({ where: { id } })
+
+    return user
+  } catch {
+    return null
+  }
+}
+
+export const getUserByUsername = async (username: string) => {
+  try {
+    const user = await prismadb.user.findUnique({ where: { username } })
+
+    return user
+  } catch {
+    return null
+  }
+}
