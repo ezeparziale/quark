8a0b82ad6eb91b732b1575d220818b5236b59f90
Ezequiel Parziale <eparziale@gmail.com>
2023-12-31 00:03:53 -0300

    :lipstick: improve: navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,9 +12,9 @@ const navigation: INavigation[] = [
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-muted bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <nav
-        className="container flex h-16 max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8"
+        className="container flex h-14 max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8"
         aria-label="Global"
       >
         <DesktopNav navigation={navigation} />
