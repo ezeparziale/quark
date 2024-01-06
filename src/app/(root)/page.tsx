b81942245c37183ff998ade01c0c15ca832f36ee
Ezequiel Parziale <eparziale@gmail.com>
2024-01-05 21:36:44 -0300

    :lipstick: update: improve sections

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,34 +7,36 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <>
-      <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="flex flex-col items-center space-y-4 text-center">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">
-              Welcome to{" "}
-              <span className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 bg-clip-text text-transparent">
-                Quark
-              </span>
-            </h1>
-            <p className="max-w-[600px] text-foreground md:text-xl">
-              Your template for Next.js to create tools faster
-            </p>
-            <section className="flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-              <Button size={"lg"} asChild>
-                <Link href="/auth/login">
-                  Get Started <ArrowRight className="ml-3 h-5 w-5" />
-                </Link>
-              </Button>
-              <Button size={"lg"} variant={"outline"} asChild>
-                <Link href="https://github.com/ezeparziale/quark">
-                  <Github className="mr-3 h-5 w-5" />
-                  GitHub
-                </Link>
-              </Button>
-            </section>
-          </div>
+      <div className="container">
+        <div className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+          <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">
+            Welcome to{" "}
+            <span className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 bg-clip-text text-transparent">
+              Quark
+            </span>
+          </h1>
+          <p className="max-w-[750px] text-balance text-center text-lg text-muted-foreground sm:text-xl">
+            Your template for Next.js to create tools faster.
+          </p>
+          <section className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+            <Button size={"sm"} asChild>
+              <Link href="/auth/login">
+                Get Started <ArrowRight className="ml-3 h-4 w-4" />
+              </Link>
+            </Button>
+            <Button size={"sm"} variant={"outline"} asChild>
+              <Link
+                href="https://github.com/ezeparziale/quark"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Github className="mr-3 h-4 w-4" />
+                GitHub
+              </Link>
+            </Button>
+          </section>
         </div>
-      </section>
+      </div>
     </>
   )
 }
