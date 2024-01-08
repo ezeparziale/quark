565300ed9a993c3b214c512d73a2672343c160db
Ezequiel Parziale <eparziale@gmail.com>
2024-01-08 20:15:49 -0300

    :zap: improve: container

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,15 @@
-export default function Container({ children }: { children: React.ReactNode }) {
+import { cn } from "@/lib/utils"
+
+export default function Container({
+  children,
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="container mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <div
+      className={cn("container mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8", className)}
+      {...props}
+    >
       {children}
     </div>
   )
