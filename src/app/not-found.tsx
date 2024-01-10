9bb55d3a5754529b971621ad959376c6d146832f
Ezequiel Parziale <eparziale@gmail.com>
2024-01-09 21:28:01 -0300

    :lipstick: update: add show logo

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,13 @@ import Container from "@/components/container"
 export default function NotFoundPage() {
   return (
     <>
-      <Container className="mt-14 border-t border-transparent">
-        <NotFound404 message="Oops! Page not found" link="/" linkText="Back to home" />
+      <Container className="mt-20">
+        <NotFound404
+          message="Oops! Page not found"
+          link="/"
+          linkText="Back to home"
+          showLogo
+        />
       </Container>
     </>
   )
