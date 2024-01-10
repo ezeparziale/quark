385181111a6b06dc2c55cf5f5a2df689b6454d7e
Ezequiel Parziale <eparziale@gmail.com>
2024-01-10 19:14:17 -0300

    :lipstick: update: change layout loading

--- src/app/(admin)/admin/roles/[roleId]/loading.tsx
+++ src/app/(admin)/admin/roles/[roleId]/loading.tsx
@@ -6,13 +6,11 @@ export default function LoadingPage() {
     <div className="space-y-6">
       <div className="space-y-0.5">
         <Skeleton className="h-[28px] w-64" />
-        <Skeleton className="mt-[2px] h-[20px] w-96" />
+        <Skeleton className="h-[20px] w-96" />
       </div>
       <Separator className="my-6" />
-      <div className="flex w-full flex-col space-y-8 md:w-2/3">
-        <div>
-          <Skeleton className="h-64 w-full" />
-        </div>
+      <div className="flex w-full flex-col">
+        <Skeleton className="h-64 w-auto" />
       </div>
     </div>
   )
