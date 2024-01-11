8099f0c596191e9c241c9321beb1621997ecf90c
Ezequiel Parziale <eparziale@gmail.com>
2024-01-11 20:55:51 -0300

    :lipstick: update: remove padding

--- src/components/ui/data-tables/data-table-pagination.tsx
+++ src/components/ui/data-tables/data-table-pagination.tsx
@@ -15,7 +15,7 @@ interface DataTablePaginationProps<TData> {
 
 export function DataTablePagination<TData>({ table }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between space-x-2 py-4">
+    <div className="flex items-center justify-between px-2">
       <div className="flex items-center space-x-2">
         <div className="flex w-[100px] items-center justify-start text-sm font-medium">
           Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
