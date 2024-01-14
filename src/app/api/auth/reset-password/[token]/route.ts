bc65ed886c472d3490bc3c5629aabef566db6215
Ezequiel Parziale <eparziale@gmail.com>
2024-01-14 10:15:29 -0300

    :fire: update: remove unused get endpoint

--- src/app/api/auth/reset-password/[token]/route.ts
+++ src/app/api/auth/reset-password/[token]/route.ts
@@ -5,25 +5,6 @@ import { verifyUserToken } from "@/lib/jwt"
 import prismadb from "@/utils/prismadb"
 import bcrypt from "bcrypt"
 
-export async function GET(req: Request, { params }: { params: { token: string } }) {
-  try {
-    const { token } = params
-
-    const email = verifyUserToken(token)
-
-    if (!email) {
-      return NextResponse.json(
-        { message: "Token is invalid or expired" },
-        { status: 401 },
-      )
-    }
-
-    return NextResponse.json({ message: "Token valid" }, { status: 200 })
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 })
-  }
-}
-
 export async function POST(req: Request, { params }: { params: { token: string } }) {
   try {
     const body = await req.json()
