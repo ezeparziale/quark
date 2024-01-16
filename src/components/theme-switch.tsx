9b32b604e21c69c1474a487179cacee4bdfc50d1
Ezequiel Parziale <eparziale@gmail.com>
2024-01-15 21:11:20 -0300

    :lipstick: update: change theme icon to radix icons version

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react"
 
-import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { RxMoon, RxSun } from "react-icons/rx"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -29,8 +29,8 @@ export default function ThemeSwitch() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <Sun className="h-[1.0rem] w-[1.0rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.0rem] w-[1.0rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <RxSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <RxMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
