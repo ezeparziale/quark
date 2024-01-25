436710e889c14527e023514ca599d42bdafc529c
Ezequiel Parziale <eparziale@gmail.com>
2024-01-25 20:23:22 -0300

    :lipstick: update: set max screen size

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -7,7 +7,10 @@ export default function Container({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("container mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8", className)}
+      className={cn(
+        "container mx-auto max-w-screen-2xl px-4 py-6 sm:px-6 lg:px-8",
+        className,
+      )}
       {...props}
     >
       {children}
