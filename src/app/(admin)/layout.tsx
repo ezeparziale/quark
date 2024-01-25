5da31aba659f6aad260bace561b7f594f3d804ff
Ezequiel Parziale <eparziale@gmail.com>
2024-01-25 20:55:34 -0300

    :lipstick: update: add header nav admin

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,10 +1,25 @@
+import HeaderHav from "@/components/admin/header-nav"
 import Container from "@/components/container"
 import Navbar from "@/components/navbar/navbar"
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export interface INavigation {
+  title: string
+  href: string
+  type: "parent" | "child"
+}
+
+const navigation: INavigation[] = [
+  { title: "Overview", href: "/admin", type: "parent" },
+  { title: "Users", href: "/admin/users", type: "child" },
+  { title: "Roles", href: "/admin/roles", type: "child" },
+  { title: "Permissions", href: "/admin/permissions", type: "child" },
+]
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
+      <HeaderHav items={navigation} />
       <main>
         <Container>{children}</Container>
       </main>
