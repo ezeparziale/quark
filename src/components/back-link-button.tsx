d20f910dd2276e35421f02abc1c06e6ab6a1f72c
Ezequiel Parziale <eparziale@gmail.com>
2024-01-25 20:17:16 -0300

    :lipstick: update: set margin top default

--- src/components/back-link-button.tsx
+++ src/components/back-link-button.tsx
@@ -8,7 +8,7 @@ import { Button } from "./ui/button"
 
 export default function BackLinkButton({ link }: { link: string }) {
   return (
-    <Button size="sm" variant="link" className="mb-6 p-0 text-muted-foreground" asChild>
+    <Button size="sm" variant="link" className="mt-1 p-0 text-muted-foreground" asChild>
       <Link href={link}>
         <ArrowLeft className="h-4 w-4" />
         <span className="text-sm">Back</span>
