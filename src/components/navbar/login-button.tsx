bdd751b9f8e2191186d6f4c9b6ac295753f06d4c
Ezequiel Parziale <eparziale@gmail.com>
2024-02-08 22:30:35 -0300

    :zap: improve: login button to server component

--- src/components/navbar/login-button.tsx
+++ src/components/navbar/login-button.tsx
@@ -1,26 +1,8 @@
-"use client"
-
 import Link from "next/link"
-
-import React, { Dispatch, SetStateAction } from "react"
-
-import { useSession } from "next-auth/react"
 
 import { Button } from "@/components/ui/button"
 
-import UserNav from "./user-nav"
-
-export default function LoginButton({
-  setOpenSheet,
-}: {
-  setOpenSheet?: Dispatch<SetStateAction<boolean>>
-}) {
-  const { data: session, status } = useSession()
-
-  if (status === "authenticated") {
-    return <UserNav setOpenSheet={setOpenSheet} email={session.user.email!} />
-  }
-
+export default function LoginButton() {
   return (
     <div className="flex space-x-1">
       <Button asChild variant="outline" size="default">
