2fd2389dd9cbfef1dbc5fd787a9eb96a5682cc5d
Ezequiel Parziale <eparziale@gmail.com>
2024-02-08 22:39:35 -0300

    :zap: improve: desktop and mobile navs

--- src/components/navbar/desktop-nav.tsx
+++ src/components/navbar/desktop-nav.tsx
@@ -5,46 +5,49 @@ import { usePathname } from "next/navigation"
 
 import { cn } from "@/lib/utils"
 import { SlashIcon } from "lucide-react"
-import { useSession } from "next-auth/react"
 
 import Logo from "../logo"
 import ThemeSwitch from "../theme-switch"
 import LoginButton from "./login-button"
 import { INavigation } from "./navbar"
 import ToolSwitcher from "./tool-switcher"
+import UserNav from "./user-nav"
 
 export default function DesktopNav({ navigation }: { navigation: INavigation[] }) {
   const pathname = usePathname()
-  const { data: session } = useSession()
+  const isHome = pathname === "/" || pathname === "/about"
 
   return (
     <>
-      <div className="hidden items-center lg:flex lg:space-x-6">
-        <Logo />
-        {!session &&
-          navigation.map((item) => (
-            <Link
-              key={`menu_lg_${item.name}`}
-              href={item.href}
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-primary",
-                pathname != item.href && "text-muted-foreground",
-              )}
-            >
-              {item.name}
-            </Link>
-          ))}
-        {session && (
-          <>
-            <SlashIcon className="mx-3 hidden size-4 -rotate-[15deg] text-foreground/10 md:block" />
-            <ToolSwitcher />
-          </>
-        )}
-      </div>
-      <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <ThemeSwitch />
-        <LoginButton />
-      </div>
+      <nav className="hidden h-16 items-center justify-between px-6 lg:flex lg:flex-1">
+        <div className="flex items-center space-x-6 pr-6">
+          <Logo />
+          {isHome &&
+            navigation.map((item) => (
+              <Link
+                key={`menu_lg_${item.name}`}
+                href={item.href}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-primary",
+                  pathname != item.href && "text-muted-foreground",
+                )}
+              >
+                {item.name}
+              </Link>
+            ))}
+          {!isHome && (
+            <>
+              <SlashIcon className="mx-3 hidden size-4 -rotate-[15deg] text-foreground/10 md:block" />
+              <ToolSwitcher />
+            </>
+          )}
+        </div>
+        <div className="flex space-x-3">
+          <ThemeSwitch />
+          {isHome && <LoginButton />}
+          {!isHome && <UserNav />}
+        </div>
+      </nav>
     </>
   )
 }
--- src/components/navbar/mobile-nav.tsx
+++ src/components/navbar/mobile-nav.tsx
@@ -6,38 +6,36 @@ import { usePathname } from "next/navigation"
 import { useState } from "react"
 
 import { cn } from "@/lib/utils"
-import { Menu } from "lucide-react"
-import { useSession } from "next-auth/react"
+import { Menu, SlashIcon } from "lucide-react"
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
 import Logo from "../logo"
 import ThemeSwitch from "../theme-switch"
 import LoginButton from "./login-button"
 import { INavigation } from "./navbar"
 import ToolSwitcher from "./tool-switcher"
+import UserNav from "./user-nav"
 
 export default function MobileNav({ navigation }: { navigation: INavigation[] }) {
   const pathname = usePathname()
 
   const [openSheet, setOpenSheet] = useState(false)
 
-  const { data: session } = useSession()
+  const isHome = pathname === "/" || pathname === "/about"
 
   return (
-    <div className="flex flex-grow items-center justify-between lg:hidden">
+    <nav className="flex h-16 flex-1 items-center justify-between px-6 lg:hidden">
       <div className="flex items-center justify-between">
         <Logo />
-        {session && <ToolSwitcher />}
+        {!isHome && (
+          <>
+            <SlashIcon className="ml-12 size-4 -rotate-[15deg] text-foreground/10 md:mx-3" />
+            <ToolSwitcher />
+          </>
+        )}
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center space-x-1">
         <ThemeSwitch />
         <Sheet open={openSheet} onOpenChange={setOpenSheet}>
           <SheetTrigger asChild aria-controls="radix-:Ribdd9j9:">
@@ -50,37 +48,35 @@ export default function MobileNav({ navigation }: { navigation: INavigation[] })
             </button>
           </SheetTrigger>
           <SheetContent className="fixed inset-y-0 right-0 w-full overflow-y-auto border-0 sm:border-l-2">
-            <SheetHeader>
-              <SheetTitle className="flex justify-start">
-                <Logo />
-              </SheetTitle>
-            </SheetHeader>
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <SheetClose asChild key={item.name}>
-                      <Link
-                        key={`sheet_close_${item.name}`}
-                        href={item.href}
-                        className={cn(
-                          "-mx-3 block px-3 py-2 text-sm font-medium transition-colors hover:text-primary",
-                          pathname != item.href && "text-muted-foreground",
-                        )}
-                      >
-                        {item.name}
-                      </Link>
-                    </SheetClose>
-                  ))}
-                </div>
+                {isHome && (
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <SheetClose asChild key={item.name}>
+                        <Link
+                          key={`sheet_close_${item.name}`}
+                          href={item.href}
+                          className={cn(
+                            "-mx-3 block px-3 py-2 text-sm font-medium transition-colors hover:text-primary",
+                            pathname != item.href && "text-muted-foreground",
+                          )}
+                        >
+                          {item.name}
+                        </Link>
+                      </SheetClose>
+                    ))}
+                  </div>
+                )}
                 <div className="py-6">
-                  <LoginButton setOpenSheet={setOpenSheet} />
+                  {isHome && <LoginButton />}
+                  {!isHome && <UserNav setOpenSheet={setOpenSheet} />}
                 </div>
               </div>
             </div>
           </SheetContent>
         </Sheet>
       </div>
-    </div>
+    </nav>
   )
 }
