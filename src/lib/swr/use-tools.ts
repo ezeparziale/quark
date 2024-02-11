f271223dce7e336dfa58179d276e482426f158eb
Ezequiel Parziale <eparziale@gmail.com>
2024-02-11 20:06:12 -0300

    :recycle: refactor: add fetcher for swr

--- src/lib/swr/use-tools.ts
+++ src/lib/swr/use-tools.ts
@@ -2,25 +2,7 @@ import { ITools } from "@/types/types"
 import { TOOLS } from "@/utils"
 import useSWR from "swr"
 
-interface SWRError extends Error {
-  status: number
-}
-
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit,
-): Promise<JSON> {
-  const res = await fetch(input, init)
-
-  if (!res.ok) {
-    const error = await res.text()
-    const err = new Error(error) as SWRError
-    err.status = res.status
-    throw err
-  }
-
-  return res.json()
-}
+import { fetcher } from "./fetcher"
 
 export default function useTools() {
   const { data, error, isLoading } = useSWR<ITools[]>("/api/tools", fetcher)
