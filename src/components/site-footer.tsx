daa26ec87c9d58094f1a9ba24d7680dcf5f70ca9
Ezequiel Parziale <eparziale@gmail.com>
2024-02-13 19:12:47 -0300

    :rocket: update: add site footer

---
+++ src/components/site-footer.tsx
@@ -0,0 +1,21 @@
+import { CommandMenu } from "./command-menu"
+import Logo from "./logo"
+import MaxWidthWrapper from "./max-width-wrapper"
+
+export function SiteFooter() {
+  return (
+    <footer className="border-t">
+      <MaxWidthWrapper className="py-6 md:px-8 md:py-0">
+        <div className="flex flex-row items-center justify-between md:h-24 md:flex-row">
+          <div className="mt-3 flex justify-center space-x-3">
+            <Logo disableScale disableName />
+            <p className="text-sm">2024</p>
+          </div>
+          <div className="flex justify-center space-x-3">
+            <CommandMenu />
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </footer>
+  )
+}
