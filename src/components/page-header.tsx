da9d970ec7ece2abd8a4edbb022f2ce2a8a1ce8b
Ezequiel Parziale <eparziale@gmail.com>
2024-03-24 20:23:03 -0300

    :recycle: refactor: page header

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -4,33 +4,6 @@ import BackLinkButton from "./back-link-button"
 import { CopyButtonData } from "./copy-clipboard-button"
 import { Separator } from "./ui/separator"
 
-interface PageHeaderContentProps {
-  title: string
-  description: string
-  copy?: string
-}
-
-function PageHeaderContent({ title, description, copy }: PageHeaderContentProps) {
-  return (
-    <div className="space-y-0.5">
-      <h2 className="text-xl font-bold tracking-tight md:text-2xl">{title}</h2>
-      <div className="flex items-center justify-start">
-        <p className="text-sm text-muted-foreground md:text-base">{description}</p>
-        {copy && <CopyButtonData textToCopy={copy} />}
-      </div>
-    </div>
-  )
-}
-
-function PageSectionContent({ title, description }: PageHeaderContentProps) {
-  return (
-    <div>
-      <h3 className="text-lg font-medium">{title}</h3>
-      <p className="text-sm text-muted-foreground">{description}</p>
-    </div>
-  )
-}
-
 interface PageHeaderProps {
   title: string
   description: string
@@ -55,7 +28,13 @@ export function PageHeader({
           linkBack ? "mt-4" : "mt-14",
         )}
       >
-        <PageHeaderContent title={title} description={description} copy={copy} />
+        <div className="space-y-0.5">
+          <h2 className="text-xl font-bold tracking-tight md:text-2xl">{title}</h2>
+          <div className="flex items-center justify-start">
+            <p className="text-sm text-muted-foreground md:text-base">{description}</p>
+            {copy && <CopyButtonData textToCopy={copy} />}
+          </div>
+        </div>
         {action}
       </div>
       <Separator className="my-6" />
@@ -74,7 +53,10 @@ export function PageSection({ title, description, action }: PageSectionProps) {
     <>
       <div className="mb-6 space-y-6">
         <div className="flex items-center justify-between">
-          <PageSectionContent title={title} description={description} />
+          <div>
+            <h3 className="text-lg font-medium">{title}</h3>
+            <p className="text-sm text-muted-foreground">{description}</p>
+          </div>
           {action}
         </div>
         <Separator />
