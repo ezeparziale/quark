8ff3e2d251652a400a0fde9fff99b187ac8d43ba
Ezequiel Parziale <eparziale@gmail.com>
2024-04-01 09:38:15 -0300

    :zap: improve: page header

--- src/app/(admin)/admin/permissions/new/page.tsx
+++ src/app/(admin)/admin/permissions/new/page.tsx
@@ -4,16 +4,16 @@ import { PageHeader } from "@/components/page-header"
 
 import PermissionForm from "../_components/permission-form"
 
-const title = "Create new permission"
-const description = "Create a permission which can be assigned to your roles."
-const linkBack = "/admin/permissions"
-
 export default async function NewPermissionPage() {
   await protectPage({ permission: "admin:all" })
 
   return (
     <>
-      <PageHeader title={title} description={description} linkBack={linkBack} />
+      <PageHeader
+        title="Create new permission"
+        description="Create a permission which can be assigned to your roles."
+        linkBack="/admin/permissions"
+      />
       <PermissionForm />
     </>
   )
