bc8506004088e45175daf4455ea82ec955c7f2aa
Ezequiel Parziale <eparziale@gmail.com>
2024-04-02 11:19:08 -0300

    :lipstick: update: improve skeleton loading

--- src/app/(admin)/admin/roles/new/loading.tsx
+++ src/app/(admin)/admin/roles/new/loading.tsx
@@ -1,31 +1,27 @@
-import { Separator } from "@/components/ui/separator"
+import { PageHeader } from "@/components/page-header"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export default function LoadingCreateRolePage() {
   return (
     <div className="space-y-6">
       <Skeleton className="mb-6 h-8 w-16 p-0" />
-      <div className="space-y-0.5">
-        <Skeleton className="h-8 w-64" />
-        <Skeleton className="mt-[2px] h-6 w-96" />
-      </div>
-      <Separator className="my-6" />
-      <div className="flex w-full flex-col space-y-8 md:w-2/3">
-        <div>
+      <PageHeader.Skeleton />
+      <div className="mt-6 flex w-full flex-col space-y-8 md:w-2/3">
+        <div className="space-y-2">
           <Skeleton className="h-5 w-24" />
-          <Skeleton className="mt-2 h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
         </div>
-        <div>
+        <div className="space-y-2">
           <Skeleton className="h-5 w-24" />
-          <Skeleton className="mt-2 h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
         </div>
-        <div>
+        <div className="space-y-2">
           <Skeleton className="h-5 w-24" />
-          <Skeleton className="mt-2 h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
         </div>
         <div className="mt-4 flex flex-col space-y-4">
-          <Skeleton className="h-9 w-full rounded-md px-3 md:w-1/5" />
-          <Skeleton className="h-9 w-full rounded-md px-3 md:w-1/5" />
+          <Skeleton className="h-9 w-full md:w-1/5" />
+          <Skeleton className="h-9 w-full md:w-1/5" />
         </div>
       </div>
     </div>
