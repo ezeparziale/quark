b22963e759af529e351ac56d3942bfc4c8b84acc
Ezequiel Parziale <eparziale@gmail.com>
2024-04-05 19:31:41 -0300

    :rocket: update: add current user tools

--- src/app/api/tools/route.ts
+++ src/app/api/tools/route.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server"
 
-import { TOOLS } from "@/utils"
+import { getCurrentUser } from "@/actions/users/get-current-user"
+import { getUserTools } from "@/data/user"
 
-export async function GET(req: Request) {
-  return NextResponse.json(TOOLS, { status: 200 })
+export async function GET() {
+  const currentUser = await getCurrentUser()
+
+  if (currentUser) {
+    const data = await getUserTools(currentUser.id)
+
+    return NextResponse.json(data, { status: 200 })
+  }
+
+  return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
 }
