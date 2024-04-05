a208ede0e133a43a3aed424089ae7e79059575fc
Ezequiel Parziale <eparziale@gmail.com>
2024-04-05 19:30:22 -0300

    :rocket: update: add get user tools query

--- src/data/user.ts
+++ src/data/user.ts
@@ -29,3 +29,51 @@ export const getUserByUsername = async (username: string) => {
     return null
   }
 }
+
+export const getUserTools = async (userId: string, search?: string) => {
+  try {
+    const data = await prismadb.user.findUnique({
+      where: { id: userId },
+      select: {
+        roles: {
+          select: {
+            role: {
+              select: {
+                tools: {
+                  select: {
+                    tool: { select: { id: true, name: true, href: true, icon: true } },
+                  },
+                },
+              },
+            },
+          },
+        },
+        toolsFavorites: true,
+      },
+    })
+
+    if (!data) return null
+
+    const favoritesToolIds = data?.toolsFavorites.map((tool) => tool.toolId)
+
+    const flatTools = data?.roles.map((role) =>
+      role.role.tools.map((tool) => tool.tool),
+    )
+
+    const toolsData = flatTools
+      ?.flat()
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((tool) => ({ ...tool, isFavorite: favoritesToolIds.includes(tool.id) }))
+
+    if (search) {
+      const filteredData = toolsData?.filter((tool) =>
+        tool.name.toLowerCase().includes(search.toLowerCase()),
+      )
+      return filteredData
+    }
+
+    return toolsData
+  } catch {
+    return null
+  }
+}
