2c71fb5cedc8584858d2e3f59a8910c20066057f
Ezequiel Parziale <eparziale@gmail.com>
2024-04-07 00:07:40 -0300

    :rocket: update: add tool empty state

---
+++ src/app/(tools)/tools/_components/tool-empty-state.tsx
@@ -0,0 +1,15 @@
+import { LayoutGrid } from "lucide-react"
+
+import EmptyState from "@/components/ui/data-tables/empty-state"
+
+export default function ToolEmptyState() {
+  return (
+    <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed p-14 shadow-sm">
+      <EmptyState
+        title="No results found"
+        description="Your search did not return any results."
+        icon={LayoutGrid}
+      />
+    </div>
+  )
+}
--- src/app/(tools)/tools/_components/tools-section.tsx
+++ src/app/(tools)/tools/_components/tools-section.tsx
@@ -1,6 +1,7 @@
 import { getCurrentUser } from "@/actions/users/get-current-user"
 import { getUserTools } from "@/data/user"
 
+import ToolEmptyState from "./tool-empty-state"
 import ToolsCards from "./tools-cards"
 
 export default async function ToolsSection({
@@ -15,6 +16,10 @@ export default async function ToolsSection({
   const data = (await getUserTools(currentUser?.id!, search)) || []
 
   const favoriteTools = data?.filter((tool) => tool.isFavorite === true) || []
+
+  if (!data.length) {
+    return <ToolEmptyState />
+  }
 
   return <ToolsCards tools={data} favorites={favoriteTools} view={view} />
 }
