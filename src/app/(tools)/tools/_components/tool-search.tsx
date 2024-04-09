16346edd1d762f398067fc1c34b49c5532c9e578
Ezequiel Parziale <eparziale@gmail.com>
2024-04-09 19:26:26 -0300

    :lipstick: update: improve search input
    - add loading icon
    - add button clear search

--- src/app/(tools)/tools/_components/tool-search.tsx
+++ src/app/(tools)/tools/_components/tool-search.tsx
@@ -2,37 +2,82 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
-import { SearchIcon } from "lucide-react"
+import { useRef, useTransition } from "react"
+
+import { Loader2, SearchIcon, XCircleIcon } from "lucide-react"
 import { useDebouncedCallback } from "use-debounce"
 
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 
 export default function ToolSearch() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
+  const [isSearching, startTransition] = useTransition()
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const q = searchParams.get("q")?.toString()
 
   const handleSearch = useDebouncedCallback((query: string) => {
-    const params = new URLSearchParams(searchParams)
-    if (query) {
-      params.set("q", query)
-    } else {
-      params.delete("q")
-    }
-    replace(`${pathname}?${params.toString()}`)
+    startTransition(() => {
+      const params = new URLSearchParams(searchParams)
+      if (query) {
+        params.set("q", query)
+      } else {
+        params.delete("q")
+      }
+      replace(`${pathname}?${params.toString()}`)
+    })
   }, 300)
+
+  const handleClearInput = () => {
+    if (inputRef.current) {
+      inputRef.current.value = ""
+      handleSearch("")
+    }
+  }
 
   return (
     <div className="relative flex w-full min-w-40 items-center gap-1.5">
-      <SearchIcon className="absolute left-4 size-5 text-muted-foreground" />
+      {isSearching ? (
+        <Loader2 className="absolute left-4 size-5 animate-spin text-muted-foreground" />
+      ) : (
+        <SearchIcon className="absolute left-4 size-5 text-muted-foreground" />
+      )}
+
       <Input
         className="w-full p-5 pl-12"
         placeholder="Search..."
         onChange={(e) => {
           handleSearch(e.target.value)
         }}
-        defaultValue={searchParams.get("q")?.toString()}
+        defaultValue={q}
+        onKeyDown={(e) => {
+          if (e.key === "Escape") {
+            inputRef?.current?.blur()
+          }
+        }}
+        ref={inputRef}
       />
+      {q && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              className="absolute right-1"
+              onClick={handleClearInput}
+              variant={"ghost"}
+              size={"icon"}
+            >
+              <XCircleIcon className="size-5 text-muted-foreground" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Clear search</p>
+          </TooltipContent>
+        </Tooltip>
+      )}
     </div>
   )
 }
