18677fea8a98dd984e03a7a94d8bb39580fcefbf
Ezequiel Parziale <eparziale@gmail.com>
2024-04-11 20:00:48 -0300

    :lipstick: update: add variant theme switch for footer

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useState } from "react"
 
+import { cn } from "@/lib/utils"
 import { useTheme } from "next-themes"
-import { RxMoon, RxSun } from "react-icons/rx"
+import { RxDesktop, RxMoon, RxSun } from "react-icons/rx"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -13,9 +14,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export default function ThemeSwitch() {
+export default function ThemeSwitch({ isDropDown = true }: { isDropDown?: boolean }) {
   const [mounted, setMounted] = useState(false)
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   useEffect(() => {
     setMounted(true)
@@ -25,20 +26,47 @@ export default function ThemeSwitch() {
     return null
   }
 
+  if (isDropDown) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <RxSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <RxMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <RxSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <RxMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <div className="flex flex-row items-center space-x-1 rounded-full border p-1">
+        <button
+          className={cn("p-1", theme === "light" && "rounded-full bg-muted")}
+          onClick={() => setTheme("light")}
+        >
+          <RxSun className="size-4 transition-all" />
+        </button>
+        <button
+          className={cn("p-1", theme === "system" && "rounded-full bg-muted")}
+          onClick={() => setTheme("system")}
+        >
+          <RxDesktop className="size-4 transition-all" />
+        </button>
+        <button
+          className={cn("p-1", theme === "dark" && "rounded-full bg-muted")}
+          onClick={() => setTheme("dark")}
+        >
+          <RxMoon className="size-4 transition-all" />
+        </button>
+      </div>
+    </>
   )
 }
