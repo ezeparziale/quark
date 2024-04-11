bea807dd8853c6cf917d2ae79d5fe39fce13d060
Ezequiel Parziale <eparziale@gmail.com>
2024-04-11 20:00:19 -0300

    :lipstick: update: improve command menu with button action

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -26,6 +26,8 @@ import {
   CommandSeparator,
 } from "@/components/ui/command"
 
+import { Button } from "./ui/button"
+
 interface IQuickLinks {
   icon: LucideIcon
   label: string
@@ -74,12 +76,14 @@ export function CommandMenu() {
 
   return (
     <>
-      <p className="text-sm text-muted-foreground">
-        Command menu{" "}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>K
-        </kbd>
-      </p>
+      <Button variant={"ghost"} onClick={() => setOpen(true)} className="p-1">
+        <p className="text-sm text-muted-foreground">
+          Command menu{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border-2 bg-muted px-1.5 font-mono font-medium text-muted-foreground">
+            <span className="text-xs">⌘</span>K
+          </kbd>
+        </p>
+      </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder="Type a command or search..." />
         <CommandList>
