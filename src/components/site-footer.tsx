2011220595604dc0e683a61d4b2c862176d1f913
Ezequiel Parziale <eparziale@gmail.com>
2024-04-11 20:02:37 -0300

    :lipstick: update: add health status and theme switch

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,6 +1,8 @@
 import { CommandMenu } from "./command-menu"
+import HealthStatusButton from "./health-status-button"
 import Logo from "./logo"
 import MaxWidthWrapper from "./max-width-wrapper"
+import ThemeSwitch from "./theme-switch"
 
 export function SiteFooter() {
   return (
@@ -10,9 +12,13 @@ export function SiteFooter() {
           <div className="flex items-center justify-center space-x-3">
             <Logo disableScale disableName />
             <p className="text-sm">2024</p>
+            <HealthStatusButton />
           </div>
-          <div className="flex justify-center space-x-3">
-            <CommandMenu />
+          <div className="flex items-center justify-between space-x-3">
+            <div className="hidden justify-center space-x-3 lg:flex">
+              <CommandMenu />
+            </div>
+            <ThemeSwitch isDropDown={false} />
           </div>
         </div>
       </MaxWidthWrapper>
