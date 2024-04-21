9ea626e83bd6608f71287f4e17503d00c4db360a
Ezequiel Parziale <eparziale@gmail.com>
2024-04-21 17:34:23 -0300

    :rocket: update: add getToolById

---
+++ src/data/tools.ts
@@ -0,0 +1,11 @@
+import prismadb from "@/lib/prismadb"
+
+export const getToolById = async (id: number) => {
+  try {
+    const tool = await prismadb.tool.findUnique({ where: { id } })
+
+    return tool
+  } catch {
+    return null
+  }
+}
