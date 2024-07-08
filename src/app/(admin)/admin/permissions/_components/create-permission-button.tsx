e7fcbada6a959d81768692048d5633e65b63b85f
Ezequiel Parziale <eparziale@gmail.com>
2024-07-08 20:30:29 -0300

    :rocket: feat: add permission with dialog

--- src/app/(admin)/admin/permissions/_components/create-permission-button.tsx
+++ src/app/(admin)/admin/permissions/_components/create-permission-button.tsx
@@ -1,25 +1,204 @@
-import Link from "next/link"
+"use client"
 
-import { Plus } from "lucide-react"
+import { useEffect, useState } from "react"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Permission } from "@prisma/client"
+import { Loader2, Plus } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import * as z from "zod"
+
+import { addServerErrors } from "@/lib/utils"
+
+import { permissionSchema } from "@/schemas/permissions"
+
+import { createPermission, updatePermission } from "@/actions/permissions"
 
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  ResponsiveDialog,
+  ResponsiveDialogClose,
+  ResponsiveDialogContent,
+  ResponsiveDialogDescription,
+  ResponsiveDialogFooter,
+  ResponsiveDialogHeader,
+  ResponsiveDialogTitle,
+  ResponsiveDialogTrigger,
+} from "@/components/ui/responsive-dialog"
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 
-export default function CreatePermissionButton() {
+type FormData = z.infer<typeof permissionSchema>
+
+export default function CreatePermissionButton({
+  permission,
+}: {
+  permission?: Permission
+}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const form = useForm<FormData>({
+    defaultValues: {
+      id: permission?.id || undefined,
+      name: permission?.name || "",
+      description: permission?.description || "",
+      key: permission?.key || "",
+    },
+    resolver: zodResolver(permissionSchema),
+  })
+
+  const action = permission ? "Update" : "Create"
+
+  const onSubmitCreate = async (data: FormData) => {
+    const result = await createPermission(data)
+    if (result.success) {
+      setIsOpen(false)
+      toast.success("Permission created successfully!")
+    } else {
+      if (result.errors) {
+        addServerErrors(result.errors, form.setError)
+      } else if (result.message) {
+        toast.error(result.message)
+      } else {
+        toast.error("Something went wrong")
+      }
+    }
+  }
+
+  const onSubmitUpdate = async (data: FormData) => {
+    const result = await updatePermission(data)
+    if (result.success) {
+      form.reset({ ...data })
+      setIsOpen(false)
+      toast.success("Permission updated successfully!")
+    } else {
+      if (result.errors) {
+        addServerErrors(result.errors, form.setError)
+      } else if (result.message) {
+        toast.error(result.message)
+      } else {
+        toast.error("Something went wrong")
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen === false) {
+      form.reset()
+    }
+  }, [isOpen, form])
+
   return (
-    <Tooltip>
-      <TooltipTrigger>
-        <Button asChild>
-          <Link href="/admin/permissions/new">
-            <Plus className="size-4" />
-            <span className="sr-only">create permission</span>
-            <span className="ml-2 hidden md:block">Create permission</span>
-          </Link>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent className="md:hidden" align={"end"}>
-        <p>Create permission</p>
-      </TooltipContent>
-    </Tooltip>
+    <ResponsiveDialog open={isOpen} onOpenChange={setIsOpen}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <ResponsiveDialogTrigger asChild>
+            <Button>
+              <Plus className="size-4" />
+              <span className="sr-only">create permission</span>
+              <span className="ml-2 hidden md:block">Create permission</span>
+            </Button>
+          </ResponsiveDialogTrigger>
+        </TooltipTrigger>
+        <TooltipContent className="md:hidden" align={"end"}>
+          <p>Create permission</p>
+        </TooltipContent>
+      </Tooltip>
+      <ResponsiveDialogContent className="sm:max-w-[525px]">
+        <ResponsiveDialogHeader className="text-left">
+          <ResponsiveDialogTitle>Create new permission</ResponsiveDialogTitle>
+          <ResponsiveDialogDescription>
+            Create a permission which can be assigned to your roles.
+          </ResponsiveDialogDescription>
+        </ResponsiveDialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={
+              action === "Create"
+                ? form.handleSubmit(onSubmitCreate)
+                : form.handleSubmit(onSubmitUpdate)
+            }
+            className="flex w-full flex-col gap-4 px-4 sm:px-0"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="e.g. Create posts"
+                      {...field}
+                      disabled={form.formState.isSubmitting}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="key"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Key</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="feature:action e.g. posts:create"
+                      {...field}
+                      disabled={form.formState.isSubmitting}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="e.g. A user who is allowed to create a post"
+                      {...field}
+                      disabled={form.formState.isSubmitting}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <ResponsiveDialogFooter className="flex flex-col-reverse px-0 pt-0 sm:flex-row">
+              <ResponsiveDialogClose asChild>
+                <Button variant="outline" type="button">
+                  Cancel
+                </Button>
+              </ResponsiveDialogClose>
+              <Button
+                disabled={form.formState.isSubmitting || !form.formState.isDirty}
+                type="submit"
+              >
+                {form.formState.isSubmitting && (
+                  <Loader2 className="mr-2 size-4 animate-spin" />
+                )}
+                Create permission
+              </Button>
+            </ResponsiveDialogFooter>
+          </form>
+        </Form>
+      </ResponsiveDialogContent>
+    </ResponsiveDialog>
   )
 }
