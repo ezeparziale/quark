d1cfa11cc994de909320814949b01f4e07ea5185
Ezequiel Parziale <eparziale@gmail.com>
2024-07-24 21:04:48 -0300

    :rotating_light: fix: typo

--- src/env.mjs
+++ src/env.mjs
@@ -3,7 +3,7 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
-    // Enviorement
+    // Environment
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     // Database
     DATABASE_URL: z.string().url(),
