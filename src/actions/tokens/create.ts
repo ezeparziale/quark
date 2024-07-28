3b85a5ed960e4627f9dae7077add6fc34cd98a26
Ezequiel Parziale <eparziale@gmail.com>
2024-07-28 18:47:49 -0300

    :rocket: chore: add token actions

---
+++ src/actions/tokens/create.ts
@@ -0,0 +1,46 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+
+import { auth } from "@/auth"
+import * as z from "zod"
+
+import { DataResult } from "@/types/types"
+
+import { createTokenApi } from "@/lib/auth"
+import prismadb from "@/lib/prismadb"
+import { has } from "@/lib/rbac"
+import { validateSchemaAction } from "@/lib/validate-schema-action"
+
+import { tokenCreateSchema } from "@/schemas/tokens"
+
+type FormData = z.infer<typeof tokenCreateSchema>
+
+async function handler(formData: FormData): Promise<DataResult<FormData>> {
+  try {
+    const isAuthorized = await has({ role: "admin" })
+
+    if (!isAuthorized) {
+      return { success: false, message: "Unauthorized" }
+    }
+
+    const session = await auth()
+
+    const { name } = formData
+
+    const { token, hashedToken, partialToken } = await createTokenApi()
+
+    await prismadb.token.create({
+      data: { name, hashedToken, partialToken, userId: session?.user.userId! },
+    })
+
+    revalidatePath(`/admin/tokens/`)
+
+    return { success: true, data: { token } }
+  } catch (error) {
+    console.error("Error creating token:", error)
+    return { success: false, message: "Something went wrong" }
+  }
+}
+
+export const createToken = validateSchemaAction(tokenCreateSchema, handler)
