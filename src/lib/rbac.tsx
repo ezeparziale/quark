80c2184ca808edf866a9653626837ab97b972e05
Ezequiel Parziale <eparziale@gmail.com>
2024-07-30 19:18:14 -0300

    :zap: chore: improve rbac query

--- src/lib/rbac.tsx
+++ src/lib/rbac.tsx
@@ -14,7 +14,7 @@ type Opts = RolesCheck | PermissionsCheck
 async function getUserRoles(userId: number) {
   const userRoles = await prismadb.userRole
     .findMany({
-      where: { userId },
+      where: { userId, user: { id: userId, active: true } },
       select: {
         role: {
           select: {
@@ -31,7 +31,7 @@ async function getUserRoles(userId: number) {
 async function getUserPermissions(userId: number) {
   const userPermissions = await prismadb.userRole
     .findMany({
-      where: { userId },
+      where: { userId, user: { id: userId, active: true } },
       select: {
         role: {
           select: {
