952ccbc5f092b6525f94974148fc86b9f803173c
Ezequiel Parziale <eparziale@gmail.com>
2024-09-21 16:32:20 -0300

    :rocket: chore: improve link edit button

--- src/app/(admin)/admin/permissions/_components/cell-actions.tsx
+++ src/app/(admin)/admin/permissions/_components/cell-actions.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 import { useState } from "react"
 
@@ -18,7 +18,6 @@ import { IColumns } from "./columns"
 import DeletePermissionDialog from "./delete-permission-dialog"
 
 export default function CellActions({ row }: { row: IColumns }) {
-  const router = useRouter()
   const [isDropdownMenuOpen, setIsDropdownMenuOpen] = useState(false)
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
 
@@ -40,9 +39,11 @@ export default function CellActions({ row }: { row: IColumns }) {
             Copy ID
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => router.push(`/admin/permissions/${row.id}`)}>
-            <Pencil className="mr-2 size-4" />
-            Edit
+          <DropdownMenuItem asChild>
+            <Link href={`/admin/permissions/${row.id}`} prefetch={true}>
+              <Pencil className="mr-2 size-4" />
+              Edit
+            </Link>
           </DropdownMenuItem>
           <DropdownMenuItem
             onSelect={() => {
