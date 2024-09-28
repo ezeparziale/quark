560db6a51aafc3d609718117668a8263239df252
Ezequiel Parziale <eparziale@gmail.com>
2024-09-28 18:27:39 -0300

    :zap: chore: improve loading user new page

--- src/app/(admin)/admin/users/new/loading.tsx
+++ src/app/(admin)/admin/users/new/loading.tsx
@@ -7,22 +7,42 @@ export default function LoadingCreateUserPage() {
     <div className="space-y-6">
       <Skeleton className="mb-6 h-8 w-16 p-0" />
       <PageHeader.Skeleton />
-      <div className="mt-6 flex w-full flex-col space-y-8 md:w-2/3">
-        <div className="space-y-2">
-          <Skeleton className="h-5 w-24" />
-          <Skeleton className="h-10 w-full" />
+      <div className="mt-8 flex w-full flex-col space-y-8 md:w-2/3">
+        <div className="flex gap-x-4">
+          <div className="flex-1 space-y-2">
+            <Skeleton className="h-5 w-12" />
+            <Skeleton className="h-11 w-full" />
+            <Skeleton className="h-4 w-40" />
+          </div>
+          <div className="space-y-2">
+            <Skeleton className="h-5 w-12" />
+            <div className="flex h-10 items-center justify-center">
+              <Skeleton className="h-6 w-11 rounded-full" />
+            </div>
+          </div>
         </div>
         <div className="space-y-2">
-          <Skeleton className="h-5 w-24" />
-          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-5 w-20" />
+          <Skeleton className="h-11 w-full" />
+          <Skeleton className="h-4 w-48" />
         </div>
-        <div className="space-y-2">
-          <Skeleton className="h-10 w-48" />
+        <div className="space-y-9">
+          <div className="flex flex-row items-center justify-between rounded-lg border p-5">
+            <div className="space-y-0.5">
+              <Skeleton className="h-4 w-28" />
+              <Skeleton className="h-5 w-64" />
+            </div>
+            <Skeleton className="h-6 w-11 rounded-full" />
+          </div>
+          <div className="flex flex-row items-center justify-between rounded-lg border p-5">
+            <div className="space-y-0.5">
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="h-5 w-64" />
+            </div>
+            <Skeleton className="h-6 w-11 rounded-full" />
+          </div>
         </div>
-        <div className="space-y-2">
-          <Skeleton className="h-10 w-48" />
-        </div>
-        <div className="mt-4 flex flex-col space-y-4">
+        <div className="flex flex-col space-y-4">
           <Skeleton className="h-9 w-full md:w-1/5" />
           <Skeleton className="h-9 w-full md:w-1/5" />
         </div>
