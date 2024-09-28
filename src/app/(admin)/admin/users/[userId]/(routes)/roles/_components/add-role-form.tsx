fa37123bf32f32b28e4853209827def13d903d80
Ezequiel Parziale <eparziale@gmail.com>
2024-09-28 18:28:31 -0300

    :zap: chore: rename button update to save

--- src/app/(admin)/admin/users/[userId]/(routes)/roles/_components/add-role-form.tsx
+++ src/app/(admin)/admin/users/[userId]/(routes)/roles/_components/add-role-form.tsx
@@ -108,7 +108,7 @@ export default function AddRoleForm({ options, selectedValues, title, userId }: 
             {form.formState.isSubmitting && (
               <Loader2 className="mr-2 size-4 animate-spin" />
             )}
-            Update
+            Save
           </Button>
           <Button size="sm" className="w-full md:w-1/5" variant="outline" asChild>
             <Link href={`/admin/users/${userId}/roles`}>Cancel</Link>
