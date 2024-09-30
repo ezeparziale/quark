a709572db72045caf9617f30a94dd4907d726a7c
Ezequiel Parziale <eparziale@gmail.com>
2024-09-30 19:33:20 -0300

    :zap: chore: improve section loading admin

--- src/components/admin/section-loading.tsx
+++ src/components/admin/section-loading.tsx
@@ -1,13 +1,11 @@
 import { PageSection } from "../page-header"
-import { Separator } from "../ui/separator"
 import TableLoading from "./table-loading"
 
 export default function SectionLoading() {
   return (
-    <div className="space-y-6">
+    <>
       <PageSection.Skeleton />
-      <Separator className="my-6" />
       <TableLoading />
-    </div>
+    </>
   )
 }
