52fa77aa2ce73e999cc71ed82f46b2696503ce9a
Ezequiel Parziale <eparziale@gmail.com>
2024-09-30 19:32:27 -0300

    :zap: chore: add aria label

--- src/components/copy-clipboard-button.tsx
+++ src/components/copy-clipboard-button.tsx
@@ -25,9 +25,13 @@ export function CopyButtonData({ textToCopy, className, ...props }: IProps) {
     <div className={cn("flex items-center justify-end", className)} {...props}>
       <Tooltip>
         <TooltipTrigger asChild>
-          <Button variant="secondary" className="mx-2 h-6 p-1.5" onClick={onCopy}>
+          <Button
+            variant="secondary"
+            className="mx-2 h-6 p-1.5"
+            onClick={onCopy}
+            aria-label={`Copy ${textToCopy}`}
+          >
             {isCopied ? <Check className="size-3" /> : <Copy className="size-3" />}
-            <span className="sr-only">Copy</span>
           </Button>
         </TooltipTrigger>
         <TooltipContent>Copy</TooltipContent>
