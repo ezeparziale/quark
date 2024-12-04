80b59bfc86106b5234bd549beef72c833292aa9f
Ezequiel Parziale <eparziale@gmail.com>
2024-12-03 21:56:15 -0300

    :rocket: chore: add format date util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
+import { formatDistanceToNow } from "date-fns"
+import { format, formatInTimeZone, toZonedTime } from "date-fns-tz"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -32,4 +34,30 @@ export const getSearchParams = (url: string) => {
   })
 
   return params
+}
+
+export const formatDate = (date: Date) => {
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const localDate = toZonedTime(date, userTimeZone)
+
+  const utcDateTime = formatInTimeZone(
+    new Date(date.toISOString()),
+    "UTC",
+    "MMMM d, yyyy HH:mm:ss",
+  )
+
+  const localDateTime = format(localDate, "MMMM d, yyyy HH:mm:ss", {
+    timeZone: userTimeZone,
+  })
+
+  const gmtOffset = format(localDate, "zzz", {
+    timeZone: userTimeZone,
+  })
+
+  return {
+    timeAgo: formatDistanceToNow(date, { addSuffix: true }),
+    utcDateTime,
+    localDateTime,
+    localTimeZone: gmtOffset,
+  }
 }
