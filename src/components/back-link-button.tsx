85b7f15cb72af5284ef3461bb6501cbc463b71c2
Ezequiel Parziale <eparziale@gmail.com>
2025-03-25 19:18:24 -0300

    :art: refactor: replace ChevronLeft icon with ArrowLeft in BackLinkButton component

--- src/components/back-link-button.tsx
+++ src/components/back-link-button.tsx
@@ -2,15 +2,20 @@ import Link from "next/link"
 
 import React from "react"
 
-import { ChevronLeft } from "lucide-react"
+import { ArrowLeft } from "lucide-react"
 
 import { Button } from "./ui/button"
 
 export default function BackLinkButton({ link }: { link: string }) {
   return (
-    <Button size="sm" variant="link" className="text-muted-foreground mt-1 p-0" asChild>
+    <Button
+      size="sm"
+      variant="link"
+      className="text-muted-foreground mt-1 has-[>svg]:px-0"
+      asChild
+    >
       <Link href={link}>
-        <ChevronLeft className="size-4" />
+        <ArrowLeft className="size-4" />
         <span className="text-sm">Back</span>
       </Link>
     </Button>
