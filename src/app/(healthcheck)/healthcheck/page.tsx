29091ba6e5f33a0aedc76e5c54f320b7c1320ce1
Ezequiel Parziale <eparziale@gmail.com>
2025-03-26 20:48:47 -0300

    :rocket: feat: add healthcheck page with under construction message and navigation

---
+++ src/app/(healthcheck)/healthcheck/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { ArrowLeft } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+import Logo from "@/components/logo"
+import MaxWidthWrapper from "@/components/max-width-wrapper"
+
+export default function Healthcheck() {
+  return (
+    <MaxWidthWrapper>
+      <div className="mt-20 flex flex-col items-center">
+        <Logo disableScale />
+        <Image
+          alt="missing site"
+          src={`/illustrations/page-under-construction-light.svg`}
+          width={400}
+          height={400}
+          className="dark:hidden"
+        />
+        <Image
+          alt="missing site"
+          src={`/illustrations/page-under-construction-dark.svg`}
+          width={400}
+          height={400}
+          className="hidden dark:block"
+        />
+        <h2 className="text-2xl font-bold tracking-tight md:text-3xl">
+          Under construction
+        </h2>
+        <Button asChild variant="default" size="sm" className="mt-6">
+          <Link href="/">
+            <ArrowLeft className="mr-2 size-4" />
+            Back to home
+          </Link>
+        </Button>
+      </div>
+    </MaxWidthWrapper>
+  )
+}
