f07be9ec11692f56abd5e5cbad07f25ec03c4669
Ezequiel Parziale <eparziale@gmail.com>
2025-03-29 16:11:47 -0300

    :bug: fix: import geist font mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Geist } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google"
 
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -17,6 +17,11 @@ const geistSans = Geist({
   subsets: ["latin"],
 })
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
 export const metadata: Metadata = {
   title: "Quark",
   description: "Quark template",
@@ -26,7 +31,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={cn("bg-background min-h-screen antialiased", geistSans.className)}
+        className={cn(
+          "bg-background min-h-screen antialiased",
+          geistSans.variable,
+          geistMono.variable,
+          geistSans.className,
+        )}
       >
         <div className="relative flex min-h-screen flex-col">
           <Providers>
